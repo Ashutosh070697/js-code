800bb865c22602cb633f7f17d6ad1b701abd0c38
Ashutosh070697 <129322427+Ashutosh070697@users.noreply.github.com>
2023-10-01 18:15:03 +0000

    data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,4 +13,8 @@ accountCity = "Bengaluru"
 
 // console.log(accountId);
 
+/* prefer not to use var
+becouse of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") or alert (3+3)  
+// we are using nodeJs, not browser
+
+console.log(3 
+        +
+        3) // code readability should be high
+
+console.log("Ashutosh")
+
+let name = "Ashutosh"
+let age = 26
+let isLoggedIn = false
+let state;
+
+//data types
+/*
+    number => 2 to the power of 53
+    bigInt
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => 
+    symbol => unique
+
+    object
+
+    */
+
+    console.log(typeof "Ashutosh")
+
+    console.log(typeof undefined) //undefined
+    console.log(typeof null) // object
